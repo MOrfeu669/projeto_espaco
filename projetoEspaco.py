d7f4b257b36650fe7d2c5cb50b896e8d92240f8d
Bernardo Gatto <bernardogato01@gmail.com>
2023-06-22 14:31:56 -0300

    final do codigo com a interface no canto da tela e erros corrigidos

--- projetoEspaco.py
+++ projetoEspaco.py
@@ -14,17 +14,17 @@ fundo = pg.transform.scale(fundo, tamanho)
 marcador = pg.image.load("wapoint.png")
 clock = pg.time.Clock()
 
-#carega e inicializa a musica
+# Carregando e iniciando a música
 pg.mixer.music.load('X2Download.com - C418_ Aria Math (128 kbps).mp3')
 pg.mixer.music.play(-1)
 
 # Definindo a fonte que será usada para renderizar o texto
 font = pg.font.Font(None, 24)
 
-# variavel para controlar o loop principal
+# Variável para controlar o loop principal do jogo
 running = True
 
-# Define a fonte para o rotulo de distancia
+# Definindo a fonte para o rótulo de distância
 distance_label_font = pg.font.Font(None, 30)
 distance_label = None
 
@@ -73,134 +73,134 @@ except FileNotFoundError:
     # Se o arquivo de salvamento não existir, inicia com um dicionário vazio
     star_points = {}
 
-def load_data(folder='save_data'):
-    # Dicionário para armazenar os pontos das estrelas
-    star_points = {}
-    # Cria uma janela tkinter invisível para a caixa de diálogo
-    root = tk.Tk()
-    root.withdraw()
-    # Solicita ao usuário o nome do arquivo de salvamento para carregar
-    filename = simpledialog.askstring(
-        "Load File", "Enter the name of the save file to load:")
-    # Se o usuário inseriu um nome de arquivo, carrega os pontos das estrelas do arquivo
-    if filename:
-        with open(os.path.join(folder, filename + '.txt'), 'r') as f:
-            for line in f:
-                x, y, name = line.strip().split()
-                star_points[name] = (int(x), int(y))
-    return star_points
-# carrega os pontos
-try:
-    star_points = load_data()
-except FileNotFoundError:
-    #se o arquivo de salvar não existir inicia um diconario vazio
-    star_points = {}
-# loop principal do jogo
+# Loop principal do jogo
 while running:
     for event in pg.event.get():
-        # Se o usuario fechar salva os dados e encerra o codigo
-        if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.Key == pg.K_ESCAPE):
+        # Se o usuário fechar a janela ou pressionar a tecla ESC, salva os dados e termina o loop
+        if event.type == pg.QUIT or (event.type == pg.KEYDOWN and event.key == pg.K_ESCAPE):
             save_data(star_points)
-            renning = False
-        
-        #se o usuario clicar com o mouse add um ponto
+            running = False
+
+        # Se o usuário clicar com o mouse add um ponto
         elif event.type == pg.MOUSEBUTTONDOWN:
             if tela.get_rect().collidepoint(event.pos):
-                if event.butto == 1:
-                    # cria uma janela inviseivel para a caixa de diálogo
+                if event.button == 1:
+                    # Cria uma janela tkinter invisível para a caixa de diálogo
                     root = tk.Tk()
                     root.withdraw()
 
-                    # Solicita o usuario o nomeda estrela
-                    star_name = simpledialog.askfloat(
-                        "Star name", "Enter the name of the star: ")
-                    # Se o usuario inserir o nome da estrela adiciona um ponto
+                    # Solicita ao usuário o nome da estrela
+                    star_name = simpledialog.askstring(
+                        "Star Name", "Enter the name of the star:")
+                    # Se o usuario inseriar um nome add um ponto
                     if star_name:
                         mouse_x, mouse_y = event.pos
-                        star_name[star_name] = (mouse_x, mouse_y)
-                # Se o clique foi com o botao direito 
+                        star_points[star_name] = (mouse_x, mouse_y)
+                # Se o clique foi com o botão direito do mouse
                 elif event.button == 3:
                     mouse_x, mouse_y = event.pos
                     mouse_vector = pg.math.Vector2(mouse_x, mouse_y)
 
-                    #verifica se o foi em algum marcador
+                    # Verifica se o clique foi próximo a algum marcador
                     for name, point in star_points.items():
                         marker_x, marker_y = point
                         marker_vector = pg.math.Vector2(marker_x, marker_y)
                         distance = mouse_vector.distance_to(marker_vector)
-                        #se o clique foi em um marcador, removera o mesmo
+                        # Se o clique foi próximo a um marcador, remove o marcador
                         if distance < 30:
                             del star_points[name]
                             break
 
         elif event.type == pg.KEYDOWN:
-            # Se a tecla foi f10 limpa todos marcadores tudo
+            # Se a tecla foi F10, limpa todos os marcadores
             if event.key == pg.K_F10:
                 star_points.clear()
-            # Se precionar F11, salva todos os marcadores
-            elif event.key == pg.KF11:
+            # Se a tecla foi F11, salva os marcadores
+            elif event.key == pg.K_F11:
                 save_data(star_points)
-            # Se ainda não existe um save ele inicia um dicionario vazio
-            elif event.key == pg.k_f12:
+            # Se o arquivo de salvamento não existir, inicia com um dicionário vazio
+            elif event.key == pg.K_F12:
                 try:
                     star_points = load_data()
-                except FileExistsError:
+                except FileNotFoundError:
                     star_points = {}
 
     tela.blit(fundo, (0, 0))
-    # Reseta o rotulo de distanci
+    # Reseta o rótulo de distância
     distance_label = None
-    #obtem a posicao do mouse 
+    # Obtém a posição do mouse
     mouse_x, mouse_y = pg.mouse.get_pos()
     mouse_vector = pg.math.Vector2(mouse_x, mouse_y)
     # Navega sobre os pares de pontos das estrelas
-    for i, (name1, point1) in enumerate(list(star_name.items())[:-1]):
+    for i, (name1, point1) in enumerate(list(star_points.items())[:-1]):
         name2, point2 = list(star_points.items())[i+1]
 
-        # Calcula a distancia do mouse até a nilha entre os dois pontos
+        # Calcula a distância do mouse até a linha entre os dois pontos
         line_start = pg.math.Vector2(point1)
         line_end = pg.math.Vector2(point2)
 
         # A distancia da linha for menor que 30
-        line_vector = line_end - line_end
-        t = max(0, min(1, (mouse_vector - line_start).dot(line_vector) / 
-                       line_vector.length_squared()))
+        line_vector = line_end - line_start
+        t = max(0, min(1, (mouse_vector - line_start).dot(line_vector) /
+                line_vector.length_squared()))
         projection = line_start + t * line_vector
         distance_to_line = mouse_vector.distance_to(projection)
 
-        if distance_label < 30:
+        if distance_to_line < 30:
             # Calcula o comprimento da linha
-            line_length = math.sqrt (
-                line_end.x - line_start.x) ** 2 + (line_end.y - line_start.y) ** 2
-            
-        # Cria um rotulo com o comprimento da linha
-        distance_label = distance_label_font.render(
-            f"Length: {int(line_length)} px", True, (255, 0, 0))
-        distance_to_line = (mouse_x, mouse_y)
-        break
+            line_length = math.sqrt(
+                (line_end.x - line_start.x) ** 2 + (line_end.y - line_start.y) ** 2)
 
+            # Cria um rótulo com o comprimento da linha
+            distance_label = distance_label_font.render(
+                f"Length: {int(line_length)} px", True, (255, 255, 255))
+            distance_label_pos = (mouse_x, mouse_y)
+            break
 
     for name, point in star_points.items():
-        # Obtem a cordenada do marcador
+        # Obtém as coordenadas do marcador
         marker_x, marker_y = point
-        # Cria um retangulo para o marcador com o centro nas mesmas cordenadas
-        marker_rect = marcador.get_rect(center=(marker_x,marker_y - 9))
-        # Desenah o marcador na tela
+        # Cria um retângulo para o marcador com o centro nas coordenadas do marcador
+        marker_rect = marcador.get_rect(center=(marker_x, marker_y - 9))
+        # Desenha o marcador na tela
         tela.blit(marcador, marker_rect)
 
-        # Cria uma superficie de texto com o nome da estrela
-        text_surface = font.render(name, True (255, 255, 255))
-        # Desenah o nome da estrela
-        tela.blit(text_surface, (marker_x - 15, marker_y -  38))
+        # Cria uma superfície de texto com o nome da estrela
+        text_surface = font.render(name, True, (255, 255, 255))
+        # Desenha o nome da estrela na tela
+        tela.blit(text_surface, (marker_x - 15, marker_y - 38))
 
     # Itera sobre os pares de pontos das estrelas
     for i, (name1, point1) in enumerate(list(star_points.items())[:-1]):
-        #obtem o proximo ponto da lista
+        # Obtém o próximo ponto na lista
         name2, point2 = list(star_points.items())[i+1]
         # Desenha uma linha entre os dois pontos
         pg.draw.line(tela, (255, 255, 255), point1, point2)
 
-            
+    # Cria uma fonte para as instruções
+    instructions_font = pg.font.Font(None, 29)
+    instructions_1 = instructions_font.render(
+        "Botao esquerdo: adicionar marcador", True, (255, 255, 255))
+    instructions_2 = instructions_font.render(
+        "Botao direito: excluir marcado", True, (255, 255, 255))
+    instructions_3 = instructions_font.render(
+        "Tecla F10: excluir todos marcadores", True, (255, 255, 255))
+    instructions_4 = instructions_font.render(
+        "Tecla F11: salvar marcadores", True, (255, 255, 255))
+    instructions_5 = instructions_font.render(
+        "Tecla F12: carregar marcadores", True, (255, 255, 255))
+    # Desenha as instruções na tela
+    tela.blit(instructions_1, (10, 10))
+    tela.blit(instructions_2, (10, 45))
+    tela.blit(instructions_3, (10, 80))
+    tela.blit(instructions_4, (10, 115))
+    tela.blit(instructions_5, (10, 150))
+
+    # Se existe um rótulo de distância, desenha-o na tela
+    if distance_label:
+        tela.blit(distance_label, distance_label_pos)
 
     pg.display.update()
-    clock.tick(60)
+    clock.tick(165)
+
+pg.quit()
