948178b90be12820290fe7cd21b825b813c2e4ea
Bernardo Gatto <bernardogato01@gmail.com>
2023-06-21 21:05:41 -0300

    first comit

---
+++ projetoEspaco.py
@@ -0,0 +1,98 @@
+import pygame as pg
+import tkinter as tk
+from tkinter import simpledialog
+import math
+import os
+
+# Inicializando o pygame e o mixer do pygame
+pg.init()
+pg.mixer.init()
+tamanho = (1600, 1000)
+tela = pg.display.set_mode(tamanho)
+fundo = pg.image.load("bg.jpg")
+fundo = pg.transform.scale(fundo, tamanho)
+marcador = pg.image.load("wapoint.png")
+clock = pg.time.Clock()
+
+#carega e inicializa a musica
+pg.mixer.music.load('X2Download.com - C418_ Aria Math (128 kbps).mp3')
+pg.mixer.music.play(-1)
+
+# Definindo a fonte que será usada para renderizar o texto
+font = pg.font.Font(None, 24)
+
+# variavel para controlar o loop principal
+running = True
+
+# Função para salvar os pontos das estrelas
+def save_data(star_points, folder='save_data'):
+  # Cria o diretório se ele não existir
+    if not os.path.exists(folder):
+        os.makedirs(folder)
+    # Cria uma janela tkinter invisível para a caixa de diálogo
+    root = tk.Tk()
+    root.withdraw()
+    # Solicita ao usuário o nome do arquivo de salvamento
+    filename = simpledialog.askstring(
+        "Save File", "Enter the name of the save file:")
+    # Se o usuário inseriu um nome de arquivo, salva os pontos das estrelas no arquivo
+    if filename:
+        with open(os.path.join(folder, filename + '.txt'), 'w') as f:
+            for name, point in star_points.items():
+                f.write(f'{point[0]} {point[1]} {name}\n')
+
+# Função para carregar os pontos das estrelas
+
+
+def load_data(folder='save_data'):
+    # Dicionário para armazenar os pontos das estrelas
+    star_points = {}
+    # Cria uma janela tkinter invisível para a caixa de diálogo
+    root = tk.Tk()
+    root.withdraw()
+    # Solicita ao usuário o nome do arquivo de salvamento para carregar
+    filename = simpledialog.askstring(
+        "Load File", "Enter the name of the save file to load:")
+    # Se o usuário inseriu um nome de arquivo, carrega os pontos das estrelas do arquivo
+    if filename:
+        with open(os.path.join(folder, filename + '.txt'), 'r') as f:
+            for line in f:
+                x, y, name = line.strip().split()
+                star_points[name] = (int(x), int(y))
+    return star_points
+
+
+# Tentando carregar os pontos das estrelas
+try:
+    star_points = load_data()
+except FileNotFoundError:
+    # Se o arquivo de salvamento não existir, inicia com um dicionário vazio
+    star_points = {}
+
+def load_data(folder='save_data'):
+    # Dicionário para armazenar os pontos das estrelas
+    star_points = {}
+    # Cria uma janela tkinter invisível para a caixa de diálogo
+    root = tk.Tk()
+    root.withdraw()
+    # Solicita ao usuário o nome do arquivo de salvamento para carregar
+    filename = simpledialog.askstring(
+        "Load File", "Enter the name of the save file to load:")
+    # Se o usuário inseriu um nome de arquivo, carrega os pontos das estrelas do arquivo
+    if filename:
+        with open(os.path.join(folder, filename + '.txt'), 'r') as f:
+            for line in f:
+                x, y, name = line.strip().split()
+                star_points[name] = (int(x), int(y))
+    return star_points
+# carrega os pontos
+try:
+    star_points = load_data()
+except FileNotFoundError:
+    #se o arquivo de salvar não existir inicia um diconario vazio
+    star_points = {}
+           
+    tela.blit(fundo, (0, 0))
+
+    pg.display.update()
+    clock.tick(60)
